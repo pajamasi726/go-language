449cde30053712e41869bedc8d74193bc5549838
pajamasi726 <pajamasi726@gmail.com>
2019-11-17 23:29:14 +0900

    Add exam04 if

---
+++ study/exam04-if/src/exam04-if.go
@@ -0,0 +1,16 @@
+package main
+
+func main()  {
+
+	var age = 10
+	var now = 1
+
+	if current:=age-now; current == 10 {
+		println("10살")
+	} else if age > 10{
+		println("10살 이상")
+	} else{
+		println("10살 이하")
+	}
+
+}
